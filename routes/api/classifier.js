f4f25297770928893f02cabeded14cdfa0a8aee0
Foxyf76 <lukefox5@gmail.com>
2019-11-18 18:27:46 +0000

    Working example! Wohoo!

--- routes/api/classifier.js
+++ routes/api/classifier.js
@@ -1,14 +1,17 @@
-global.fetch = require('node-fetch');
-
 const axios = require('axios');
-var fs = require('fs');
-
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const express = require('express');
 const router = express.Router();
 const tf = require('@tensorflow/tfjs-node');
 require('@tensorflow/tfjs-node');
 const tfImage = require('@teachablemachine/image');
 const modelPaths = require('../../classification/paths');
+
+// needed to overcome teachablemachine dom requirements
+const dom = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
+global.fetch = require('node-fetch');
+global.document = dom.window.document;
 
 let maleClothedModel, femaleClothedModel;
 
@@ -20,8 +23,6 @@ const getTensor3dObject = async inputImage => {
     responseType: 'arraybuffer'
   });
 
-  console.log('AXIOS\n');
-  console.log(req.data);
   return tf.node.decodeJpeg(req.data, 3);
 };
 
@@ -40,7 +41,7 @@ router.post('/male_clothed_tf_image', async (req, res) => {
   );
 
   let converted = await getTensor3dObject(req.body.image);
-  model.predict(converted);
+  console.log(await model.predict(converted));
 });
 
 module.exports = router;
