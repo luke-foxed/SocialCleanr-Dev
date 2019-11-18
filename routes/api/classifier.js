9ca1e9c8694ec54fefe1760bcdf960de3565ad62
Foxyf76 <lukefox5@gmail.com>
2019-11-18 22:36:49 +0000

    actual working soultion at last

--- routes/api/classifier.js
+++ routes/api/classifier.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const jsdom = require('jsdom');
+const { Image, createCanvas } = require('canvas');
 const { JSDOM } = jsdom;
 const express = require('express');
 const router = express.Router();
@@ -7,8 +8,6 @@ const tf = require('@tensorflow/tfjs-node');
 require('@tensorflow/tfjs-node');
 const tfImage = require('@teachablemachine/image');
 const modelPaths = require('../../classification/paths');
-
-const toUint8array = require('base64-to-uint8array');
 const mobilenet = require('@tensorflow-models/mobilenet');
 
 // needed to overcome teachablemachine dom requirements
@@ -27,32 +26,33 @@ const getTensor3dObject = async inputImage => {
   return tf.node.decodeJpeg(req.data, 3);
 };
 
-router.post('/male_clothed_tf', async (req, res) => {
-  const newModel = await tf.loadLayersModel(modelUrl + 'model.json');
-
-  let converted = await getTensor3dObject(req.body.image);
-
-  newModel.predict(converted);
-});
-
 router.post('/male_clothed_tf_image', async (req, res) => {
-  const model = await tfImage.load(
+  const generatedModel = await tfImage.load(
     modelUrl + 'model.json',
     modelUrl + 'metadata.json'
   );
 
-  const maxPredictions = model.getTotalClasses();
-  console.log(maxPredictions);
-
   const mobilenetModel = await mobilenet.load({
-    version: 1,
+    version: 2,
     alpha: 0.25 | 0.5 | 0.75 | 1.0
   });
 
   let converted = await getTensor3dObject(req.body.image);
 
-  console.log(await model.predict(converted));
-  console.log(await mobilenetModel.classify(converted));
+  console.log('My Model: \n' + (await generatedModel.predict(converted)));
+  console.log('Mobilenet: \n' + (await mobilenetModel.classify(converted)));
+
+  // working solution -->
+  // need to cut canvas
+  let canvas = createCanvas(600, 600);
+  let ctx = canvas.getContext('2d');
+
+  let img = new Image();
+  img.onload = async () => {
+    await ctx.drawImage(img, 0, 0, 600, 600);
+    res.send(await generatedModel.predict(canvas));
+  };
+  img.src = req.body.image;
 });
 
 module.exports = router;
