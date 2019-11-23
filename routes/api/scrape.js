eba20dc43e27ec4aff3cededb4b4a2d746ea0e6f
Foxyf76 <lukefox5@gmail.com>
2019-11-23 18:17:04 +0000

    Further work on scraper authentication

--- routes/api/scrape.js
+++ routes/api/scrape.js
@@ -1,6 +1,7 @@
 const scraper = require('../scraper');
 const express = require('express');
 const router = express.Router();
+const config = require('config');
 
 /**
 @route   GET api/scrape
@@ -8,10 +9,18 @@ const router = express.Router();
 @access  Public
 */
 
-router.get('/', async (req, res) => {
-  scraper.login().then(data => {
-    console.log(data);
-  });
+router.post('/', async (req, res) => {
+  // let { email, password, authCode } = req.body;
+  // scraper.login(email, password, authCode).then(data => {
+  //   console.log(data);
+  // });
+
+  const login = await scraper.loginFacebook(
+    config.facebookEmail,
+    config.facebookPassword,
+    req.body.authCode
+  );
+  res.send(login);
 });
 
 module.exports = router;
--- routes/scraper.js
+++ routes/scraper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 const config = require('config');
 
-const login = async () => {
+const authenticated = false;
+
+const loginFacebook = async (email, password, authCode) => {
   successfulLogin = false;
   try {
     var browser = await puppeteer.launch({ headless: false });
@@ -9,15 +11,23 @@ const login = async () => {
 
     await page.goto('http://www.facebook.com/login');
 
-    await page.focus('#email');
-    await page.keyboard.type(config.facebookEmail);
-    await page.focus('#pass');
-    await page.keyboard.type(config.facebookPassword);
+    await page.type('#email', email, { delay: 15 });
+    await page.type('#pass', password, { delay: 15 });
+    await page.click('#loginbutton', { delay: 30 });
 
-    await page.click('#loginbutton');
+    await page.waitForNavigation();
+    await page.type('#approvals_code', authCode);
+
+    await page.click('#checkpointSubmitButton', { delay: 30 });
+    await page.click('#checkpointSubmitButton');
+
+    let header = await page.$(
+      "//li[@id='navItem_100006292571516']/a[@class='_5afe' and 1]/div[@class='linkWrap noCount' and 1]"
+    );
+    return header.getProperty('textContent');
   } catch (e) {
     console.log('Error: ' + e);
   }
 };
 
-module.exports.login = login;
+module.exports.loginFacebook = loginFacebook;
