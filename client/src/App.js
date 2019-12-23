3fb1cee7a6e650941fade3b142239b28baaba9b7
Foxyf76 <lukefox5@gmail.com>
2019-12-23 12:23:54 +0000

    Merge fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,9 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
 import Landing from './views/Landing';
 import Login from './components/Auth/Login';
 import Dashboard from './views/Dashboard/Dashboard';
 import Home from './views/SidebarPages/Home';
-=======
-import Landing from './components/layout/Landing';
-import Login from './components/Auth/Login';
-import Dashboard from './components/Dashboard/Dashboard';
->>>>>>> 34bebe36b0a261940ce2eb897d61819483be19c9
 
 const App = () => (
   <Router>
