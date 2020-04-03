475d7420eff024fc81426dca86a45b6ae6411d71
Luke Fox <lukefox5@gmail.com>
2020-04-03 12:29:21 +0100

    Addid registration tests

---
+++ test/authentication.js
@@ -0,0 +1,88 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
+const dummyAccounts = require('./dummyAccounts');
+chai.use(chaiHttp);
+
+// store token for deletion
+let accountToken = '';
+
+describe('Registration', () => {
+  it('Should create a new account successfully', done => {
+    chai
+      .request(server)
+      .post('/api/auth/register')
+      .send(dummyAccounts.validAccount)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('string');
+        accountToken = res.body;
+        done();
+      });
+  });
+
+  it('Should reject attempt to register with a used email', done => {
+    chai
+      .request(server)
+      .post('/api/auth/register')
+      .send(dummyAccounts.duplicateEmailAccount)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.errors[0].msg.should.equal(
+          'This User already exists, try logging in'
+        );
+        done();
+      });
+  });
+
+  it('Should trigger all validation errors', done => {
+    chai
+      .request(server)
+      .post('/api/auth/register')
+      .send(dummyAccounts.failAllAccount)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.errors[0].msg.should.equal('Please provide a name');
+        res.body.errors[1].msg.should.equal('Please include a valid email');
+        res.body.errors[2].msg.should.equal(
+          'Please enter a password between 8 to 24 characters'
+        );
+        res.body.errors[3].msg.should.equal(
+          'Password must contain at least one letter, special character and number'
+        );
+        done();
+      });
+  });
+
+  it('Should reject registration using weak password', done => {
+    chai
+      .request(server)
+      .post('/api/auth/register')
+      .send(dummyAccounts.weakPasswordAccount)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.errors[0].msg.should.equal(
+          'Please enter a password between 8 to 24 characters'
+        );
+        res.body.errors[1].msg.should.equal(
+          'Password must contain at least one letter, special character and number'
+        );
+        done();
+      });
+  });
+
+  it('Should delete an account', done => {
+    chai
+      .request(server)
+      .delete('/api/user/delete')
+      .set('x-auth-token', accountToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
---
+++ test/dummyAccounts.js
@@ -0,0 +1,32 @@
+const validAccount = {
+  name: 'Jane Doe',
+  email: 'jane@doe.com',
+  password: '!iubdfiubeDD22'
+};
+
+const duplicateEmailAccount = {
+  name: 'John Doe',
+  email: 'jane@doe.com',
+  password: '!h!huawddfiAWE131'
+};
+
+// will trigger all validations when registering
+const failAllAccount = {
+  name: '',
+  email: 'test',
+  password: '123456'
+};
+
+// missing special characters and length
+const weakPasswordAccount = {
+  name: 'John Doe',
+  email: 'john@doe.com',
+  pass: 'If663J'
+};
+
+module.exports = {
+  validAccount,
+  duplicateEmailAccount,
+  failAllAccount,
+  weakPasswordAccount
+};
