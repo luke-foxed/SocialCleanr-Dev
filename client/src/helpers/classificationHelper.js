8acb01d5fd8ac3e42274bfb974af10eee528ab29
Luke Fox <lukefox5@gmail.com>
2020-04-08 10:16:37 +0100

    Removed unused method

--- client/src/helpers/classificationHelper.js
+++ client/src/helpers/classificationHelper.js
@@ -4,84 +4,6 @@ import { getImageAsBase64 } from '../actions/scan';
 const helpers = require('./generalHelpers');
 
 /**
- * Extract flagged content from scan 
- * @param {object} results - Raw data from scan
- * @param {string} content - Content (image/text) that was passed into scan, needed when cleaning imagesld
- * @return {array} List of only flagged items
- */
-
-export const cleanResults = (results, content) => {
-  let flaggedContent = [];
-
-  // keep count of each occurance to write to DB
-  let count = {
-    flagged_text: 0,
-    flagged_clothing: 0,
-    flagged_gesture: 0,
-    flagged_age: 0
-  };
-
-  if (results['people'].length !== 0) {
-    results['people'].forEach(person => {
-      if (person.topless_prediction > 60) {
-        count.flagged_clothing++;
-        flaggedContent.push({
-          type: 'Unclothed Person',
-          message:
-            'A person with an innapropriate level of clothing has been found',
-          probability: person.topless_prediction,
-          box: person.bbox,
-          content: content
-        });
-      }
-    });
-  }
-
-  if (results['gestures'].length !== 0) {
-    results['gestures'].forEach(gesture => {
-      count.flagged_gesture++;
-      flaggedContent.push({
-        type: 'Offensive Gesture',
-        message: 'An offensive gesture has been detected in this image',
-        probability: gesture.score,
-        box: gesture.bbox,
-        content: content
-      });
-    });
-  }
-
-  if (results['text'].length !== 0) {
-    results['text'].forEach(item => {
-      count.flagged_text++;
-      flaggedContent.push({
-        type: 'Offensive Word',
-        message: `The word ${item.text} has been flagged as ${item.reason}`,
-        probability: '>80',
-        box: item.bbox,
-        content: content
-      });
-    });
-  }
-
-  if (results['age'].length !== 0) {
-    results['age'].forEach(item => {
-      if (item.age < 5) {
-        count.flagged_age++;
-        flaggedContent.push({
-          type: 'Child Detected',
-          message: `A child below the age of 5 (aged ${item.age}) has been detected`,
-          probability: item.probability,
-          box: item.bbox,
-          content: content
-        });
-      }
-    });
-  }
-
-  return { count, flaggedContent };
-};
-
-/**
  * Draw bounding box on image from URL
  * @param {string} imageURL - URL of image to draw on
  * @param {object} box - Coordinates for bounding box
@@ -94,7 +16,7 @@ export const drawBoundingBoxURL = async (imageURL, box) => {
   return image;
 };
 
- /**
+/**
  * Draw blurring box on image from URL
  * @param {string} imageURL - RL of image to draw on
  * @param {object} box - Coordinates of area to blur
@@ -127,7 +49,7 @@ export const drawBoundingBox = async (image, box) => {
   return canvasImage.toDataURL();
 };
 
- /**
+/**
  * Draw blurring box on image via canvas
  * @param {string} image - Base64 string of image
  * @param {object} box - Coordinates of area to blur
@@ -149,13 +71,13 @@ export const drawBlurringBox = async (image, box) => {
   return canvas.toDataURL();
 };
 
- /**
+/**
  * Create canvas element with image drawn on
  * @param {string} base64Image - Base64 string of image to draw on
  * @return {Canvas} Canvas element that can be used to draw bbox on
  */
 
-const createCanvasImage = async base64Image => {
+const createCanvasImage = async (base64Image) => {
   const image = await loadImage(base64Image);
   const canvas = createCanvas(image.width, image.height);
   const ctx = canvas.getContext('2d');
@@ -163,7 +85,7 @@ const createCanvasImage = async base64Image => {
   return canvas;
 };
 
- /**
+/**
  * Apply blurring to image using boxes from all flagged content by looping and layering each drawing onto image
  * @param {string} image - Base64 string of image to draw on
  * @param {array} boxes - Array of all box coordinates from each flagged item
@@ -173,19 +95,19 @@ const createCanvasImage = async base64Image => {
 export const blurAllContent = async (image, boxes) => {
   let cleanedImage = image;
 
-  await helpers.asyncForEach(boxes, async box => {
+  await helpers.asyncForEach(boxes, async (box) => {
     cleanedImage = await drawBlurringBox(cleanedImage, box);
   });
 
   return cleanedImage;
 };
 
- /**
+/**
  * Create href link containing cleaned image that is automatically clicked to download
  * @param {string} image - Base64 string of image to download
  */
 
-export const createDownloadImage = image => {
+export const createDownloadImage = (image) => {
   const a = document.createElement('a');
   a.href = image;
   a.download = 'cleaned_image.png';
